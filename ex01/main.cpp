52d56a65161e2ffc3c7f7634b8b0b508dd52231c
Hoppy <mbraets@student.42.fr>
2022-08-22 09:27:46 +0200

    ex01 - adding text to test

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,6 +5,7 @@
 
 #define PURPLE(A) "\033[35m" << A << "\033[0m"
 #define GREEN(A) "\033[92m" << A << "\033[0m"
+#define RED(A) "\033[31m" << A << "\033[0m"
 
 void	CreateBigSpan(void)
 {
@@ -12,8 +13,8 @@ void	CreateBigSpan(void)
 	Span big(10000);
 	for (int i = 0; i < 10000; ++i)
 		big.addNumber(rand() % 10000);
-	std::cout << big.shortestSpan() << std::endl;
-	std::cout << big.longestSpan() << std::endl;
+	std::cout << PURPLE("Shortest span on 10000 numbers: ") << GREEN(big.shortestSpan()) << std::endl;
+	std::cout << PURPLE("Longest span on 10000 numbers: ") << GREEN(big.longestSpan()) << std::endl;
 }
 
 int	main(void)
@@ -24,26 +25,26 @@ int	main(void)
 		err.addNumber(1);
 	}
 	catch (const std::exception & e)
-	{std::cerr << e.what() << '\n';}
+	{std::cerr << RED("Error on adding number above max: " << e.what()) << '\n';}
 	try {
 		err.shortestSpan();
 	}
 	catch (const std::exception & e)
-	{std::cerr << e.what() << '\n';}
+	{std::cerr << RED("Error on getting shortest span without 2 number: " << e.what() ) << '\n';}
 	Span sp = Span(5);
 	sp.addNumber(6);
 	sp.addNumber(3);
 	sp.addNumber(17);
 	sp.addNumber(9);
 	sp.addNumber(11);
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
+	std::cout << PURPLE("5 number shortest span: ") << GREEN(sp.shortestSpan()) << std::endl;
+	std::cout << PURPLE("5 number longest span: ") << GREEN(sp.longestSpan()) << std::endl;
 	sp = Span(5);
 	sp.addNumber(3);
 	sp.addNumber(-11);
 	sp.addNumber(3);
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
+	std::cout << PURPLE("3 number with negative shortest span: ") << GREEN(sp.shortestSpan()) << std::endl;
+	std::cout << PURPLE("3 number with negative longest span: ") << GREEN(sp.longestSpan()) << std::endl;
 	CreateBigSpan();
 	return 0;
 }
